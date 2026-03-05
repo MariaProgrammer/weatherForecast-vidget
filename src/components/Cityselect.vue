9ef6417b7b3e18c9695791364611b146a6d311eb
MariaProgrammer <maryfrontend22@gmail.com>
2026-03-05 22:41:00 +0300

    if-else condition added

--- src/components/Cityselect.vue
+++ src/components/Cityselect.vue
@@ -1,43 +1,37 @@
 <script setup>
-import { ref } from 'vue';
-import IconLocation from "./icons/IconLocation.vue"
-import Button from "./Button.vue"
+import { ref } from "vue";
+import IconLocation from "./icons/IconLocation.vue";
+import Button from "./Button.vue";
 
 const emit = defineEmits({
   selectCity(payload) {
-    console.log(`Validation payload: ${payload}`)
-    return payload
-  }
+    console.log(`Validation payload: ${payload}`);
+    return payload;
+  },
 });
 
 let isEdited = ref(false);
 
-
 function select() {
   isEdited.value = false;
-  emit ('selectCity', 'London');
+  emit("selectCity", "London");
 }
 
 function edit() {
   isEdited.value = true;
-
 }
-
 </script>
 
 <template>
-  
-  <Button @click="edit()">
-    <IconLocation /> 
+  <div v-if="isEdited">
+    <input />
+    <Button @click="select()"> Сохранить </Button>
+  </div>
+
+  <Button v-else @click="edit()">
+    <IconLocation />
     Изменить город
   </Button>
-  <template v-if="isEdited">
-    <input/>
-  <Button @click="select()">
-    Сохранить
-  </Button>
-  </template>
-  
 </template>
 
 <style scoped></style>
